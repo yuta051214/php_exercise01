4191cc1ef056ba5004eae999084c66ebbada72e0
yuta051214 <ffftrick123@gmail.com>
2022-09-05 10:28:22 +0900

    配列と連想配列の課題完了

---
+++ 03/01_array.php
@@ -0,0 +1,9 @@
+<?php
+
+$countries = ["Japan", "America", "China"];
+
+echo $countries[0] .PHP_EOL;
+
+echo $countries[1] .PHP_EOL;
+
+echo $countries[2] .PHP_EOL;
---
+++ 03/03_array.php
@@ -0,0 +1,21 @@
+<?php
+
+$nums = [
+    [1, 2],
+    [3, 4],
+    [5, 6, 7]
+];
+
+echo $nums[1][1].PHP_EOL;
+
+echo $nums[2][2].PHP_EOL;
+
+echo $nums[2][1].PHP_EOL;
+
+echo $nums[2][0].PHP_EOL;
+
+echo $nums[0][0].PHP_EOL;
+
+echo $nums[1][0].PHP_EOL;
+
+echo $nums[0][1].PHP_EOL;
